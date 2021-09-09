078be4c907493e85d4983d9f7d354942e5cf33ff
Chandan-SS <p.chandu535@gmail.com>
2021-09-09 13:23:40 +0530

    first jenkins library

---
+++ var/odd_even.groovy
@@ -0,0 +1,30 @@
+def odd_even(int build_number){
+    if (build_number%2 == 0){
+        pipeline{
+            agents any 
+            stages{
+                stage('Even'){
+                    echo 'Build number is even'
+                    script{
+                        log.info 'Starting even build'
+                        log.warning 'Nothing to do'
+                    }
+                }
+            }
+        }
+    }
+    else {
+        pipeline {
+            agents any
+            stages {
+                stage ('Odd') {
+                    echo 'Build number is odd'
+                    script {
+                        log.info 'Starting odd build'
+                        log.warning 'Nothing to do'
+                    }
+                }
+            }
+        }
+    }
+}
